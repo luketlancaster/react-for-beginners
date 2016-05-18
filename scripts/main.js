5950aa8375bdf9dfc8aa64655c0ba82c1cece419
Luke Lancaster <luketlancaster@gmail.com>
2016-05-18 11:09:47 -0500

    lessons one and two done

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+var React    = require('react');
+var ReactDOM = require('react-dom');
+
+/*
+  StorePicker
+  This will let us make <StorePicker />
+*/
+
+var StorePicker = React.createClass({
+  render: function() {
+    return (
+      <p>lorem</p>
+    )
+  }
+
+});
+
+ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
